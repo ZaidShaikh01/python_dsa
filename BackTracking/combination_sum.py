4eb07a2526cfaf511015bced19ed2b8490bbc914
ZaidShaikh <szaid5146@gmail.com>
2025-03-16 11:51:44 +0530

    Combination Sum

---
+++ BackTracking/combination_sum.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res=[]
+        # Here will be a helper funcion
+        def helper(res,temp,start,candidates,target,sum):
+            # Base conditons
+            if sum > target:
+                return
+            if sum == target:
+                res.append(temp[:])
+            for i in range(start,len(candidates)):
+                # adding the value in temp list
+                sum+=candidates[i]
+                # adding in tempList
+                temp.append(candidates[i])
+                
+                # calling the backtrack funtion
+                helper(res,temp,i,candidates,target,sum)
+
+                # Reverting the values
+                sum-=candidates[i]
+                temp.pop()
+
+        # Here is helper funcion call
+        helper(res,[],0,candidates,target,0)
+        # Returning the res
+        return res
